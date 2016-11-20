053147c33acfd14bd98f9ce29aed9e207755fdf0
BogdanHabic <habicbogdan@gmail.com>
2016-11-20 16:59:34 +0100

    Finished if statement

--- test.c
+++ test.c
@@ -17,5 +17,15 @@ int main() {
     else
         return 5 * 5;
 
+    switch(a) {
+        case 5: 
+            return "asd";
+            break;
+        case 5: 
+            return 5 * 5;
+        default:
+            return 'd';
+    }
+
     return "";
 }
